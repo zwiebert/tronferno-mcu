117242e118fc424122f79341371f663e5126d7de
zwiebert <bertw@online.de>
2018-09-08 23:37:14 +0200

    added missing header file

---
+++ user/main/spiffs_fs.h
@@ -0,0 +1,20 @@
+/*
+ * spiffs_fs.h
+ *
+ *  Created on: 08.09.2018
+ *      Author: bertw
+ */
+
+#ifndef USER_MAIN_SPIFFS_FS_H_
+#define USER_MAIN_SPIFFS_FS_H_
+
+
+#include "spiffs_config.h"
+#include "spiffs.h"
+
+extern spiffs fs;
+#define fs_A (&fs + 0)  // fs_A - our flash file system. sizes is configured in user_config.h
+
+bool spiffs_format_fs(spiffs *fs);  // unmout, format, mount. returns success
+
+#endif /* USER_MAIN_SPIFFS_FS_H_ */
