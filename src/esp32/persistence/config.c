59ac2c30739b2e1b427c09fcd2efc2c4115cdc7c
zwiebert <bertw@online.de>
2020-02-13 19:23:47 +0100

    macros for save/restore config values

--- src/esp32/persistence/config.c
+++ src/esp32/persistence/config.c
@@ -92,32 +92,73 @@ void cfg_tz2timezone(void) {
 }
 #endif
 
-#ifdef CONFIG_BLOB
-
-void read_config(u32 mask) {
-  esp_err_t err = 0;
-  nvs_handle my_handle;
-  size_t len;
-
-  if ((err = nvs_open(CFG_NAMESPACE, NVS_READONLY, &my_handle)) == ESP_OK) {
-    err = (len = sizeof (C)), nvs_get_blob(my_handle, CFG_KEY, &C, &len);
-    nvs_close(my_handle);
-  }
-
-}
-
-void save_config(u32 mask) {
-  esp_err_t err = 0;
-  nvs_handle my_handle;
-
-  if ((err = nvs_open(CFG_NAMESPACE, NVS_READWRITE, &my_handle)) == ESP_OK) {
-    err = nvs_set_blob(my_handle, CFG_KEY, &C, sizeof (C));
-    nvs_commit(my_handle);
-    nvs_close(my_handle);
-  }
-}
-#endif
 #ifdef CONFIG_DICT
+
+static void nvsStr(nvs_handle handle, const char *key, void *dst, size_t dst_len, bool save) {
+    if (save) {
+      nvs_set_str(handle, key, dst);
+    } else {
+      nvs_get_str(handle, key, dst, &dst_len);
+    }
+}
+static void nvsBlob(nvs_handle handle, const char *key, void *dst, size_t dst_len, bool save) {
+    if (save) {
+      nvs_set_blob(handle, key, dst, dst_len);
+    } else {
+      nvs_get_blob(handle, key, dst, &dst_len);
+    }
+}
+
+#define nvs_s(mbit, key,val) if (mask&mbit) { nvsStr(handle, key, val, sizeof val, write); }
+#define nvs_b(mbit, key,val) if (mask&mbit) { nvsBlob(handle, key, &val, sizeof val, write); }
+#define nvs_i8(mbit, key,val) if (mask&mbit) { if (write) { nvs_set_i8(handle, key, val); } else { i8 temp; if (ESP_OK == nvs_get_i8(handle, key, &temp)) val = temp;}}
+#define nvs_u32(mbit, key,val) if (mask&mbit) { if (write) { nvs_set_u32(handle, key, val); } else { nvs_get_u32(handle, key, &val); }}
+
+static void rw_config(nvs_handle handle, u32 mask, bool write) {
+#ifdef USE_WLAN
+  nvs_s(CONFIG_WIFI_SSID, "C_WIFI_SSID", C.wifi_SSID);
+  nvs_s(CONFIG_WIFI_PASSWD, "C_WIFI_PASSWD", C.wifi_password);
+#endif
+#ifdef USE_NTP
+  nvs_s(CONFIG_NTP_SERVER, "C_NTP_SERVER", C.ntp_server);
+#endif
+  nvs_s(CONFIG_CFG_PASSWD, "C_CFG_PASSWD", C.app_configPassword);
+  nvs_b(CONFIG_LONGITUDE, "C_LONGITUDE", C.geo_longitude);
+  nvs_b(CONFIG_LATITUDE, "C_LATITUDE", C.geo_latitude);
+#ifndef POSIX_TIME
+    nvs_s(CONFIG_TIZO, "C_TIZO", C.geo_timezone);
+#else
+  nvs_s(CONFIG_TZ, "C_TZ", C.geo_tz);
+  if (!write)
+    cfg_tz2timezone();
+#endif
+  nvs_s(CONFIG_GPIO, "C_GPIO", C.gpio);
+#ifdef USE_MQTT
+  nvs_s(CONFIG_MQTT_URL, "C_MQTT_URL", C.mqtt_url);
+  nvs_s(CONFIG_MQTT_USER, "C_MQTT_USER", C.mqtt_user);
+  nvs_s(CONFIG_MQTT_PASSWD, "C_MQTT_PASSWD", C.mqtt_password);
+  nvs_i8(CONFIG_MQTT_ENABLE, "C_MQTT_ENABLE", C.mqtt_enable);
+#endif
+#ifdef USE_HTTP
+  nvs_s(CONFIG_HTTP_USER, "C_HTTP_USER", C.http_user);
+  nvs_s(CONFIG_HTTP_PASSWD, "C_HTTP_PASSWD", C.http_password);
+  nvs_i8(CONFIG_HTTP_ENABLE, "C_HTTP_ENABLE", C.http_enable);
+#endif
+  nvs_i8(CONFIG_RECV, "C_RECEIVER", C.app_recv);
+  nvs_i8(CONFIG_TRANSM, "C_TRANSM", C.app_transm);
+  nvs_i8(CONFIG_VERBOSE, "C_VERBOSE", C.app_verboseOutput);
+#ifdef USE_NETWORK
+  nvs_i8(CONFIG_NETWORK_CONNECTION, "C_NW_CONN", C.network);
+#endif
+#ifdef USE_LAN
+  nvs_i8(CONFIG_LAN_PHY, "C_LAN_PHY", C.lan_phy);
+  nvs_i8(CONFIG_LAN_PWR_GPIO, "C_LAN_PWR_GPIO", C.lan_pwr_gpio);
+#endif
+
+  nvs_u32(CONFIG_CUID, "C_CUID", C.fer_centralUnitID);
+  nvs_u32(CONFIG_USED_MEMBERS, "C_GMU", C.fer_usedMembers);
+  nvs_u32(CONFIG_BAUD, "C_BAUD", C.mcu_serialBaud);
+}
 
 void read_config(u32 mask) {
   esp_err_t err = 0;
@@ -125,128 +166,7 @@ void read_config(u32 mask) {
   size_t len;
 
   if ((err = nvs_open(CFG_NAMESPACE, NVS_READONLY, &handle)) == ESP_OK) {
-
-    if (mask & CONFIG_RECV) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_RECEIVER", &temp))
-        C.app_recv = temp;
-    }
-
-    if (mask & CONFIG_TRANSM) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_TRANSM", &temp))
-        C.app_transm = temp;
-    }
-
-    if (mask & CONFIG_VERBOSE) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_VERBOSE", &temp))
-        C.app_verboseOutput = temp;
-    }
-
-    if (mask & CONFIG_CUID) {
-      nvs_get_u32(handle, "C_CUID", &C.fer_centralUnitID);
-    }
-
-    if (mask & CONFIG_USED_MEMBERS) {
-      nvs_get_u32(handle, "C_GMU", &C.fer_usedMembers);
-    }
-
-    if (mask & CONFIG_BAUD) {
-      nvs_get_u32(handle, "C_BAUD", &C.mcu_serialBaud);
-    }
-
-#ifdef USE_WLAN
-    if (mask & CONFIG_WIFI_SSID) {
-      (len = sizeof C.wifi_SSID), err = nvs_get_str(handle, "C_WIFI_SSID", C.wifi_SSID, &len);
-      D(ets_printf("read wifi-ssid err=%x\n", err));
-    }
-
-    if (mask & CONFIG_WIFI_PASSWD) {
-      (len = sizeof C.wifi_password), nvs_get_str(handle, "C_WIFI_PASSWD", C.wifi_password, &len);
-    }
-#endif
-
-#ifdef USE_NTP
-    if (mask & CONFIG_NTP_SERVER) {
-      (len = sizeof C.ntp_server), err = nvs_get_str(handle, "C_NTP_SERVER", C.ntp_server, &len);
-    }
-#endif
-
-    if (mask & CONFIG_CFG_PASSWD) {
-      (len = sizeof C.app_configPassword), nvs_get_str(handle, "C_CFG_PASSWD", C.app_configPassword, &len);
-    }
-
-    if (mask & CONFIG_LONGITUDE) {
-      (len = sizeof C.geo_longitude), nvs_get_blob(handle, "C_LONGITUDE", &C.geo_longitude, &len);
-    }
-
-    if (mask & CONFIG_LATITUDE) {
-      (len = sizeof C.geo_longitude), nvs_get_blob(handle, "C_LATITUDE", &C.geo_latitude, &len);
-    }
-#ifndef POSIX_TIME
-    if (mask & CONFIG_TIZO) {
-      (len = sizeof C.geo_timezone), nvs_get_blob(handle, "C_TIZO", &C.geo_timezone, &len);
-    }
-#else
-    if (mask & CONFIG_TZ) {
-      (len = sizeof C.geo_tz), nvs_get_str(handle, "C_TZ", C.geo_tz, &len);
-      cfg_tz2timezone();
-    }
-#endif
-
-    if (mask & CONFIG_GPIO) {
-      (len = sizeof C.gpio), nvs_get_blob(handle, "C_GPIO", &C.gpio, &len);
-    }
-#ifdef USE_MQTT
-    if (mask & CONFIG_MQTT_URL) {
-      (len = sizeof C.mqtt_url), nvs_get_str(handle, "C_MQTT_URL", C.mqtt_url, &len);
-    }
-    if (mask & CONFIG_MQTT_USER) {
-      (len = sizeof C.mqtt_user), nvs_get_str(handle, "C_MQTT_USER", C.mqtt_user, &len);
-    }
-    if (mask & CONFIG_MQTT_PASSWD) {
-      (len = sizeof C.mqtt_password), nvs_get_str(handle, "C_MQTT_PASSWD", C.mqtt_password, &len);
-    }
-    if (mask & CONFIG_MQTT_ENABLE) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_MQTT_ENABLE", &temp))
-        C.mqtt_enable = temp;
-    }
-#endif
-#ifdef USE_HTTP
-    if (mask & CONFIG_HTTP_USER) {
-      (len = sizeof C.http_user), nvs_get_str(handle, "C_HTTP_USER", C.http_user, &len);
-    }
-    if (mask & CONFIG_HTTP_PASSWD) {
-      (len = sizeof C.http_password), nvs_get_str(handle, "C_HTTP_PASSWD", C.http_password, &len);
-    }
-    if (mask & CONFIG_HTTP_ENABLE) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_HTTP_ENABLE", &temp))
-        C.http_enable = temp;
-    }
-#endif
-#ifdef USE_NETWORK
-    if (mask & CONFIG_NETWORK_CONNECTION) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_NW_CONN", &temp))
-        C.network = temp;
-    }
-#endif
-#ifdef USE_LAN
-    if (mask & CONFIG_LAN_PHY) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_LAN_PHY", &temp))
-        C.lan_phy = temp;
-    }
-    if (mask & CONFIG_LAN_PWR_GPIO) {
-      i8 temp;
-      if (ESP_OK == nvs_get_i8(handle, "C_LAN_PWR_GPIO", &temp))
-        C.lan_pwr_gpio = temp;
-    }
-#endif
-
+    rw_config(handle, mask, false);
     nvs_close(handle);
   }
 
@@ -257,109 +177,7 @@ void save_config(u32 mask) {
   nvs_handle handle;
 
   if ((err = nvs_open(CFG_NAMESPACE, NVS_READWRITE, &handle)) == ESP_OK) {
-
-    if (mask & CONFIG_RECV) {
-      nvs_set_i8(handle, "C_RECEIVER", C.app_recv);
-    }
-
-    if (mask & CONFIG_TRANSM) {
-      nvs_set_i8(handle, "C_TRANSM", C.app_transm);
-    }
-
-    if (mask & CONFIG_VERBOSE) {
-      nvs_set_i8(handle, "C_VERBOSE", C.app_verboseOutput);
-    }
-
-    if (mask & CONFIG_CUID) {
-      nvs_set_u32(handle, "C_CUID", C.fer_centralUnitID);
-    }
-
-    if (mask & CONFIG_USED_MEMBERS) {
-      nvs_set_u32(handle, "C_GMU", C.fer_usedMembers);
-    }
-
-    if (mask & CONFIG_BAUD) {
-      nvs_set_u32(handle, "C_BAUD", C.mcu_serialBaud);
-    }
-
-#ifdef USE_WLAN
-    if (mask & CONFIG_WIFI_SSID) {
-      err = nvs_set_str(handle, "C_WIFI_SSID", C.wifi_SSID);
-    }
-
-    if (mask & CONFIG_WIFI_PASSWD) {
-      nvs_set_str(handle, "C_WIFI_PASSWD", C.wifi_password);
-    }
-#endif
-
-#ifdef USE_NTP
-    if (mask & CONFIG_NTP_SERVER) {
-      err = nvs_set_str(handle, "C_NTP_SERVER", C.ntp_server);
-    }
-#endif
-
-    if (mask & CONFIG_CFG_PASSWD) {
-      nvs_set_str(handle, "C_CFG_PASSWD", C.app_configPassword);
-    }
-
-   if (mask & CONFIG_LONGITUDE) {
-      nvs_set_blob(handle, "C_LONGITUDE", &C.geo_longitude, sizeof C.geo_longitude);
-    }
-
-    if (mask & CONFIG_LATITUDE) {
-      nvs_set_blob(handle, "C_LATITUDE", &C.geo_latitude, sizeof C.geo_latitude);
-    }
-#ifndef POSIX_TIME
-    if (mask & CONFIG_TIZO) {
-      nvs_set_blob(handle, "C_TIZO", &C.geo_timezone, sizeof C.geo_timezone);
-    }
-#else
-    if (mask & CONFIG_TZ) {
-      nvs_set_str(handle, "C_TZ", C.geo_tz);
-    }
-#endif
-
-    if (mask & CONFIG_GPIO) {
-       nvs_set_blob(handle, "C_GPIO", &C.gpio, sizeof C.gpio);
-     }
-#ifdef USE_MQTT
-    if (mask & CONFIG_MQTT_URL) {
-      nvs_set_str(handle, "C_MQTT_URL", C.mqtt_url);
-    }
-    if (mask & CONFIG_MQTT_USER) {
-      nvs_set_str(handle, "C_MQTT_USER", C.mqtt_user);
-    }
-    if (mask & CONFIG_MQTT_PASSWD) {
-      nvs_set_str(handle, "C_MQTT_PASSWD", C.mqtt_password);
-    }
-    if (mask & CONFIG_MQTT_ENABLE) {
-      nvs_set_i8(handle, "C_MQTT_ENABLE", C.mqtt_enable);
-    }
-#endif
-#ifdef USE_HTTP
-    if (mask & CONFIG_HTTP_USER) {
-      nvs_set_str(handle, "C_HTTP_USER", C.http_user);
-    }
-    if (mask & CONFIG_HTTP_PASSWD) {
-      nvs_set_str(handle, "C_HTTP_PASSWD", C.http_password);
-    }
-    if (mask & CONFIG_HTTP_ENABLE) {
-      nvs_set_i8(handle, "C_HTTP_ENABLE", C.http_enable);
-    }
-#endif
-#ifdef USE_NETWORK
-    if (mask & CONFIG_NETWORK_CONNECTION) {
-      nvs_set_i8(handle, "C_NW_CONN", C.network);
-    }
-#endif
-#ifdef USE_LAN
-    if (mask & CONFIG_LAN_PHY) {
-      nvs_set_i8(handle, "C_LAN_PHY", C.lan_phy);
-    }
-    if (mask & CONFIG_LAN_PWR_GPIO) {
-      nvs_set_i8(handle, "C_LAN_PWR_GPIO", C.lan_pwr_gpio);
-    }
-#endif
+    rw_config(handle, mask, true);
     nvs_commit(handle);
     nvs_close(handle);
   } else {
