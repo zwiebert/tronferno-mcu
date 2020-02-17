1c0df201d5a78324a9f75e6511538b1f8a444dcc
zwiebert <bertw@online.de>
2020-02-17 16:29:32 +0100

    fhem update added to proxy server

--- src/userio/http_data/proxy.js
+++ src/userio/http_data/proxy.js
@@ -2,18 +2,30 @@
 
 'use strict';
 
-// proxy server for testing html and js files
+const path = require('path');
+const express = require('express');
+const httpProxy = require('http-proxy');
 
-// address of the MCU
+let proj_dir=path.dirname(path.dirname(path.dirname(__dirname)));
+let tff_dir=path.dirname(proj_dir)+"/tronferno-fhem";
+let build_dir=proj_dir+"/src/esp32/build";
 let mcu = 'http://192.168.1.69:80';
-
-
-let express = require('express');
-let httpProxy = require('http-proxy');
 
 let server = express();
 
-// serving static files from current directory
+// forward any requests to MCU
+let proxy = httpProxy.createProxyServer();
+
+server.all("/*.json", (req, res) => {
+    proxy.web(req, res, { target: mcu });
+});
+
+
+// serve some files
+
+
+
+// static files of MCU HTTP server
 server.get("/index.html", (req, res) => {
     res.sendFile(__dirname + '/tfmcu_dev.html');
 });
@@ -24,20 +36,37 @@ server.get("/tfmcu_dev.js", (req, res) => {
     res.sendFile(__dirname + '/tfmcu_dev.js');
 });
 
-// forward any requests to MCU
-let proxy = httpProxy.createProxyServer();
-
-server.all("/*.json", (req, res) => {
-    proxy.web(req, res, { target: mcu });
-});
 
 
-
-
-// serve firmware image for OTA
+// ESP32 firware image for OTA update
 server.get("/tronferno-mcu.bin", (req, res) => {
-    res.sendFile('/home/bertw/proj/mcu/tronferno-mcu/src/esp32/build/tronferno-mcu.bin');
+    res.sendFile(build_dir+'/tronferno-mcu.bin');
 });
+
+
+// files for updating FHEM modules
+server.get("/tronferno-fhem/controls_tronferno.txt", (req, res) => {
+    res.sendFile(tff_dir + '/modules/tronferno/controls_tronferno.txt');
+});
+server.get("/tronferno-fhem/CHANGED", (req, res) => {
+    res.sendFile(tff_dir + '/modules/tronferno/CHANGED');
+});
+server.get("/tronferno-fhem/FHEM/00_TronfernoMCU.pm", (req, res) => {
+    res.sendFile(tff_dir + '/modules/tronferno/FHEM/00_TronfernoMCU.pm');
+});
+server.get("/tronferno-fhem/FHEM/10_Tronferno.pm", (req, res) => {
+    res.sendFile(tff_dir + '/modules/tronferno/FHEM/10_Tronferno.pm');
+});
+server.get("/fernotron-fhem/controls_fernotron.txt", (req, res) => {
+    res.sendFile(tff_dir + '/modules/sduino/controls_fernotron.txt');
+});
+server.get("/fernotron-fhem/CHANGED", (req, res) => {
+    res.sendFile(tff_dir + '/modules/sduino/CHANGED');
+});
+server.get("/fernotron-fhem/FHEM/10_Fernotron.pm", (req, res) => {
+    res.sendFile(tff_dir + '/modules/sduino/FHEM/10_Fernotron.pm');
+});
+
 
 server.listen(3000);
 
