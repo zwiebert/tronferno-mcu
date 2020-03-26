74714639466280966bc2ea2def8855c49c287901
zwiebert <bertw@online.de>
2020-03-26 22:52:51 +0100

    Add test

---
+++ src/components/fernotron/test/test_msg_tx.c
@@ -0,0 +1,59 @@
+#include "unity.h"
+#include "fer_msg_tx.h"
+
+
+
+void test_txq_repeats() {
+
+  fsbT fsb_ = { .data =  {0x80, 0xab, 0xcd, 0x19, 0x24 } };
+  fsbT *fsb = &fsb_;
+
+  TEST_ASSERT_EQUAL(0, ftx_get_msgPendingCount());
+  TEST_ASSERT_FALSE(ftx_messageToSend_isReady);
+
+  fer_send_msg(fsb, MSG_TYPE_PLAIN, 2);
+  TEST_ASSERT_EQUAL(1, ftx_get_msgPendingCount());
+
+  fer_send_msg(fsb, MSG_TYPE_PLAIN, 1);
+  TEST_ASSERT_EQUAL(2, ftx_get_msgPendingCount());
+
+  fer_tx_loop();
+  TEST_ASSERT_EQUAL(2, ftx_get_msgPendingCount());
+  TEST_ASSERT_TRUE(ftx_messageToSend_isReady);
+
+  ftx_messageToSend_isReady = false;
+  fer_tx_loop();
+  TEST_ASSERT_EQUAL(2, ftx_get_msgPendingCount());
+  TEST_ASSERT_TRUE(ftx_messageToSend_isReady);
+
+  ftx_messageToSend_isReady = false;
+  fer_tx_loop();
+  TEST_ASSERT_EQUAL(1, ftx_get_msgPendingCount());
+  TEST_ASSERT_TRUE(ftx_messageToSend_isReady);
+
+  ftx_messageToSend_isReady = false;
+  fer_tx_loop();
+  TEST_ASSERT_EQUAL(1, ftx_get_msgPendingCount());
+  TEST_ASSERT_TRUE(ftx_messageToSend_isReady);
+
+  ftx_messageToSend_isReady = false;
+  fer_tx_loop();
+  TEST_ASSERT_EQUAL(0, ftx_get_msgPendingCount());
+  TEST_ASSERT_TRUE(ftx_messageToSend_isReady);
+
+
+  fer_send_msg_with_stop(fsb, 0, 50, 0);
+  TEST_ASSERT_EQUAL(2, ftx_get_msgPendingCount());
+
+  ftx_messageToSend_isReady = false;
+  fer_tx_loop();
+  TEST_ASSERT_EQUAL(1, ftx_get_msgPendingCount());
+  TEST_ASSERT_TRUE(ftx_messageToSend_isReady);
+
+}
+
+
+TEST_CASE("tx: repeats and queue", "[fernotron]")
+{
+  test_txq_repeats();
+}
