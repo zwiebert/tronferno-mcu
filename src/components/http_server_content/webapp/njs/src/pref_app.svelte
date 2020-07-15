b612fa76b0a67da5ceb44a143b191ae2a0fcdbd9
zwiebert <bertw@online.de>
2020-07-15 22:03:04 +0200

    Add missing file

---
+++ src/components/http_server_content/webapp/njs/src/pref_app.svelte
@@ -0,0 +1,10 @@
+<script>
+  "use strict";
+  import { Verbose } from "./store/app_state.js";
+</script>
+
+<label>Verbosity:</label>
+<select bind:value={$Verbose}>
+  <option value="0">0</option>
+  <option value="1">1</option>
+</select>
