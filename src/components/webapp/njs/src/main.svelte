abac57a242dca7dcd5a8d32fe1846ee4a29849bf
zwiebert <bertw@online.de>
2020-07-20 18:05:48 +0200

    Add missing file

---
+++ src/components/webapp/njs/src/main.svelte
@@ -0,0 +1,14 @@
+<script>
+  "use strict";
+  import Layout from "./layout.svelte";
+  import Wapp from "./wapp.svelte";
+  import { setupI18n } from './services/i18n';
+
+  export let isProduction = false;
+
+
+  setupI18n();
+</script>
+
+<Layout />
+<Wapp {isProduction} />
