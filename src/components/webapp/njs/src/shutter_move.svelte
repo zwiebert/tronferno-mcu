098e9cd6580a9cb688eb9ce5940d2d97df63010a
zwiebert <bertw@online.de>
2020-07-28 22:17:22 +0200

    Improve webapp style

--- src/components/webapp/njs/src/shutter_move.svelte
+++ src/components/webapp/njs/src/shutter_move.svelte
@@ -38,15 +38,15 @@
 <div id="sdi" class="inline-block">
   <div class="flex flex-row items-center">
 
-    <button class="w-32 h-16 text-lg" type="button" on:click={hClick_Down}>
+    <button class="w-24 h-16 text-lg" type="button" on:click={hClick_Down}>
       &#x25bc;
     </button>
 
-    <button class="m-2 w-24 h-16 text-lg" type="button" on:click={hClick_Stop}>
+    <button class="m-2 w-16 h-16 text-lg" type="button" on:click={hClick_Stop}>
       STOP
     </button>
 
-    <button class="w-32 h-16 text-lg" type="button" on:click={hClick_Up}>
+    <button class="w-24 h-16 text-lg" type="button" on:click={hClick_Up}>
       &#x25b2;
     </button>
 
