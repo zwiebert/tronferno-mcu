b5b1967cd3027fe4f3a2cf0c5af978c539cb1d88
zwiebert <bertw@online.de>
2020-09-02 16:15:06 +0200

    Add mock implementations

---
+++ test/host/test_runner/mock.cc
@@ -0,0 +1,7 @@
+#include <userio_app/status_output.h>
+#include <misc/int_types.h>
+void so_output_message(so_msg_t mt, const void *arg) {}
+void  mcu_restart(void) {}
+volatile u32 run_time_s_, run_time_ts_;
+bool mcu_get_buttonPin(void) {}
+const char *config_keys[10];
