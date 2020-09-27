0f0c194b760e32977c7ba411d6ccbaba66528c12
zwiebert <bertw@online.de>
2020-09-28 00:52:02 +0200

    Fix test runner

--- comp/test/test_host/test_runner/test_runner.c
+++ comp/test/test_host/test_runner/test_runner.c
@@ -7,6 +7,30 @@
 static int idx;
 test_desc_t tests[MAX_TEST_COUNT];
 
+
+static void test_run(test_desc_t *t)
+{
+    Unity.CurrentTestName = t->desc;
+    Unity.CurrentTestLineNumber = (UNITY_LINE_TYPE)t->line;
+    Unity.TestFile = t->file;
+    Unity.NumberOfTests++;
+    UNITY_CLR_DETAILS();
+    UNITY_EXEC_TIME_START();
+    if (TEST_PROTECT())
+    {
+        //setUp();
+
+        (*t->fn)();
+    }
+    if (TEST_PROTECT())
+    {
+        //tearDown();
+    }
+    UNITY_EXEC_TIME_STOP();
+    UnityConcludeTest();
+}
+
+
 void unity_testcase_register(test_desc_t *desc) {
   assert(idx < MAX_TEST_COUNT);
 
@@ -19,8 +43,7 @@ int main() {
   for (int i = 0; i < MAX_TEST_COUNT; ++i) {
     test_desc_t *desc = &tests[i];
     if (desc->fn)
-      if (TEST_PROTECT())
-        (*desc->fn)();
+      test_run(desc);
   }
   tearDown();
   return UNITY_END();
