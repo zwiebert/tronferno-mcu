5a180413aa55c8941cf76de3bec9d6be49def241
zwiebert <bertw@online.de>
2020-11-14 13:40:07 +0100

    Improve logging

--- comp/app/app_webapp/njs/src/panes/app_log.svelte
+++ comp/app/app_webapp/njs/src/panes/app_log.svelte
@@ -1,12 +1,23 @@
 <script>
-  import { AppLogTxt } from "stores/app_log.js";
+  import { AppLogTxt, AppLog } from "stores/app_log.js";
 
   function htmlEntities(str) {
     let safeString = String(str).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
     return String(safeString).replace(/\n/g, "<br>\n");
   }
+
+  function dummy_fun(x) {
+
+  }
+  $: {
+    dummy_fun($AppLog);
+    console.log("scroll");
+    let elem = document.getElementById('app_log_div');
+    if (elem)
+       elem.scrollTop = elem.scrollHeight;
+  }
 </script>
 
-<div class="w-11/12 border-black border-2 bg-gray-400 overflow-auto h-screen">
+<div id="app_log_div" class="w-11/12 h-full border-black border-2 bg-gray-400 overflow-auto">
   {@html htmlEntities($AppLogTxt)}
 </div>
