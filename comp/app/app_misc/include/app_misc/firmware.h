b30f1861baee756972e12c1053284c5c7dbe9048
zwiebert <bertw@online.de>
2021-03-11 00:23:26 +0100

    Fix firmware URL

--- comp/app/app_misc/include/app_misc/firmware.h
+++ comp/app/app_misc/include/app_misc/firmware.h
@@ -8,13 +8,9 @@
 #ifdef USE_OTA
 #include "firmware_update/ota.h"
 
-#ifdef USE_LAN
-#define OTA_FWURL_MASTER "https://raw.githubusercontent.com/zwiebert/tronferno-mcu-bin/master/firmware/esp32/tronferno-mcu-lan.bin"
-#define OTA_FWURL_BETA "https://raw.githubusercontent.com/zwiebert/tronferno-mcu-bin/beta/firmware/esp32/tronferno-mcu-lan.bin"
-#else
+
 #define OTA_FWURL_MASTER "https://raw.githubusercontent.com/zwiebert/tronferno-mcu-bin/master/firmware/esp32/tronferno-mcu.bin"
 #define OTA_FWURL_BETA "https://raw.githubusercontent.com/zwiebert/tronferno-mcu-bin/beta/firmware/esp32/tronferno-mcu.bin"
-#endif
 
 #define OTA_FWURL_TAG_HEAD "https://raw.githubusercontent.com/zwiebert/tronferno-mcu-bin/"
 #define OTA_FWURL_TAG_TAIL "/firmware/esp32/tronferno-mcu.bin"
