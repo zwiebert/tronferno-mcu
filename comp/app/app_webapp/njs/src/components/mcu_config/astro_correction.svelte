ca1da59054dfeaf8a1ce534974d8c9f7fdda98a8
zwiebert <bertw@online.de>
2021-08-30 00:47:33 +0200

    Fix config option in webapp

--- comp/app/app_webapp/njs/src/components/mcu_config/astro_correction.svelte
+++ comp/app/app_webapp/njs/src/components/mcu_config/astro_correction.svelte
@@ -3,8 +3,8 @@
   export let value;
 </script>
 
-<select class="config-input" id="cfg_{name}" bind:value={value}>
-  <option value="0">average</option>
-  <option value="1">not too late or dark</option>
-  <option value="2">not too early or bright</option>
+<select class="config-input" id="cfg_{name}" bind:value>
+  <option value={0}>average</option>
+  <option value={1}>not too late or dark</option>
+  <option value={2}>not too early or bright</option>
 </select>
