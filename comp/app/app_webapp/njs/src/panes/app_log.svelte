90314a83fb352231d96b52f73c6d49b82b9f84c9
zwiebert <bertw@online.de>
2023-01-15 21:26:03 +0100

    Modify web colors

--- comp/app/app_webapp/njs/src/panes/app_log.svelte
+++ comp/app/app_webapp/njs/src/panes/app_log.svelte
@@ -17,13 +17,13 @@
 
 <div class="flex-row">
   Filters:
-  <label class="bg-red-400">MQTT-RX
+  <label class="bg-red-300">MQTT-RX
     <input type="checkbox" bind:checked={showMqttRx} /></label>
   <label class="bg-yellow-400">MQTT-TX
     <input type="checkbox" bind:checked={showMqttTx} /></label>
-  <label class="bg-green-400">RF-RX
+  <label class="bg-green-300">RF-RX
     <input type="checkbox" bind:checked={showRc} /></label>
-  <label class="bg-blue-400">RF-TX
+  <label class="bg-blue-300">RF-TX
     <input type="checkbox" bind:checked={showSc} /></label>
 </div>
 
@@ -36,7 +36,7 @@
     {#if 'log' in msg}
       {#if 'mqtt_client' === msg.log.tag && msg.log.txt.startsWith("receive")}
         {#if showMqttRx}
-          <span class="text-red-400">{msg.date.toLocaleTimeString() + ': ' + msg.log.tag + ': ' + msg.log.txt}<br /></span>
+          <span class="text-red-300">{msg.date.toLocaleTimeString() + ': ' + msg.log.tag + ': ' + msg.log.txt}<br /></span>
         {/if}
       {:else if 'mqtt_client' === msg.log.tag && msg.log.txt.startsWith("publish")}
         {#if showMqttTx}
@@ -48,14 +48,14 @@
 
     {:else if showRc && 'rc' in msg}
       <span
-        class="text-green-400">{msg.date.toLocaleTimeString() + ': ' + 'rx: ' + msg.rc.type + ': ' + msg.rc.a
+        class="text-green-300">{msg.date.toLocaleTimeString() + ': ' + 'rx: ' + msg.rc.type + ': ' + msg.rc.a
          + (msg.rc.type === 'central' ? '-' + msg.rc.g + msg.rc.m : '') + ' ' + msg.rc.c
          + (msg.rc.c === 'rtc' ? ' (' + msg.rc.t + ')' : '')
          + (msg.rc.c === 'auto' ? ' (' + msg.rc.t + ',' + msg.rc.f + ')' : '')
          + (msg.rc.rssi ? (':rssi=' + msg.rc.rssi) : '')}<br /></span>
     {:else if showSc && 'sc' in msg}
       <span
-        class="text-blue-400">{msg.date.toLocaleTimeString() + ': ' + 'tx: ' + msg.sc.type + ': ' + msg.sc.a 
+        class="text-blue-300">{msg.date.toLocaleTimeString() + ': ' + 'tx: ' + msg.sc.type + ': ' + msg.sc.a 
         + (msg.sc.type === 'central' ? '-' + msg.sc.g + msg.sc.m : '') + ' ' + msg.sc.c
         + (msg.sc.c === 'rtc' ? ' (' + msg.sc.t + ')' : '')
         + (msg.sc.c === 'auto' ? ' (' + msg.sc.t + ',' + msg.sc.f + ')' : '')
