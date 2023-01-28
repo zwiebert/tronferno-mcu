bb78e72658a40ba95b6960f33647dd1d322e283b
zwiebert <bertw@online.de>
2023-01-28 14:25:19 +0100

    Improve webapp

--- comp/app/app_webapp/njs/src/panes/hsconfig_fhem.svelte
+++ comp/app/app_webapp/njs/src/panes/hsconfig_fhem.svelte
@@ -14,6 +14,8 @@
   $: gmu = $Gmu;
   $: root_topic = ($McuConfig["mqtt-root-topic"] || "tfmcu") + "/";
   $: room = "Rollo";
+  $: name_prefix = "Rollo_";
+  $: alexa_name_prefix = "Rollo";
   $: clientId = ""; // ($McuConfig["mqtt-client-id"] || "tfmcu")
   $: mqttText = "";
   $: moduleText = "";
@@ -21,6 +23,9 @@
   $: {
     $Gmu;
     $Names;
+    room;
+    name_prefix;
+    alexa_name_prefix;
     root_topic;
     mqttText = genText_mqtt($Gmu);
     moduleText = genText_module($Gmu);
@@ -36,7 +41,7 @@
     return g.toString() + m.toString() + "/cmd";
   }
   function get_devName(g, m) {
-    return "Rollo_" + g.toString() + m.toString();
+    return name_prefix + g.toString() + m.toString();
   }
   function get_alias(g, m) {
     const gm = g.toString() + m.toString();
@@ -45,7 +50,7 @@
 
   function get_alexaName(g, m) {
     const gm = g.toString() + m.toString();
-    return "Rollo " + ($Names[gm] ? $Names[gm] : gm);
+    return alexa_name_prefix + " " + ($Names[gm] ? $Names[gm] : gm);
   }
 
   function genText_mqtt(gmu) {
@@ -57,25 +62,26 @@
     for (let g = 0; g < gmu.length; ++g) {
       if (g === 0 || gmu[g])
         for (let m = 0; m <= gmu[g]; ++m) {
+          const devName = get_devName(g, m);
           txt +=
-            `defmod ${get_devName(g, m)} MQTT2_DEVICE ${clientId}\n` +
-            `attr ${get_devName(g, m)} alias ${get_alias(g, m)}\n` +
-            `attr ${get_devName(g, m)} alexaName ${get_alexaName(g, m)}\n` +
-            `attr ${get_devName(g, m)} IODev mqtts\n` +
-            `attr ${get_devName(g, m)} genericDeviceType blind\n` +
-            `attr ${get_devName(g, m)} autocreate 0\n` +
-            `attr ${get_devName(g, m)} readingList ${root_topic}status:.* status\\\n` +
+            `defmod ${devName} MQTT2_DEVICE ${clientId}\n` +
+            `attr ${devName} alias ${get_alias(g, m)}\n` +
+            `attr ${devName} alexaName ${get_alexaName(g, m)}\n` +
+            `attr ${devName} room ${room}\n` +
+            `attr ${devName} IODev mqtts\n` +
+            `attr ${devName} genericDeviceType blind\n` +
+            `attr ${devName} autocreate 0\n` +
+            `attr ${devName} readingList ${root_topic}status:.* status\\\n` +
             `${root_topic + get_topic_pct_out(g, m)}:.* state\\\n` +
             `${root_topic + get_topic_pct_out(g, m)}:.* pct\n` +
-            `attr ${get_devName(g, m)} room ${room}\n` +
-            `attr ${get_devName(g, m)} setList stop:noArg ${root_topic + get_topic_cmd(g, m)} stop\\\n` +
+            `attr ${devName} setList stop:noArg ${root_topic + get_topic_cmd(g, m)} stop\\\n` +
             `up:noArg ${root_topic + get_topic_cmd(g, m)} up\\\n` +
             `down:noArg ${root_topic + get_topic_cmd(g, m)} down\\\n` +
             `sun-down:noArg ${root_topic + get_topic_cmd(g, m)} sun-down\\\n` +
             `pct:slider,0,5,100  ${root_topic + get_topic_pct(g, m)} $EVTPART1\\\n` +
             `sun-auto:1,0 ${root_topic}cli auto g=${g} m=${m} f=k  sun-auto=$EVTPART1\\\n` +
             `get_pct:noArg  ${root_topic + get_topic_pct(g, m)} ?\n` +
-            `attr ${get_devName(g, m)} webCmd up:stop:down:pct\n` +
+            `attr ${devName} webCmd up:stop:down:pct\n` +
             "\n";
         }
     }
@@ -92,13 +98,13 @@
         for (let m = 0; m <= gmu[g]; ++m) {
           const devName = get_devName(g, m);
           txt +=
-            `defmod ${get_devName(g, m)} Tronferno g=${g} m=${m}\n` +
-            `attr ${get_devName(g, m)} alias ${get_alias(g, m)}\n` +
-            `attr ${get_devName(g, m)} alexaName ${get_alexaName(g, m)}\n` +
-            `attr ${get_devName(g, m)} IODev tfmcu\n` +
-            `attr ${get_devName(g, m)} genericDeviceType blind\n` +
-            `attr ${get_devName(g, m)} room ${room}\n` +
-            `attr ${get_devName(g, m)} webCmd up:stop:down:pct\n` +
+            `defmod ${devName} Tronferno g=${g} m=${m}\n` +
+            `attr ${devName} alias ${get_alias(g, m)}\n` +
+            `attr ${devName} alexaName ${get_alexaName(g, m)}\n` +
+            `attr ${devName} room ${room}\n` +
+            `attr ${devName} IODev tfmcu\n` +
+            `attr ${devName} genericDeviceType blind\n` +
+            `attr ${devName} webCmd up:stop:down:pct\n` +
             "\n";
         }
     }
@@ -108,6 +114,12 @@
 
 <div class="main-area">
   <h4 class="text-center" use:tippy={{ content: $_("panes.hs_config.fhem.tt.header") }}>{$_("panes.hs_config.fhem.header")}</h4>
+
+  <table>
+    <tr><td>Device-Name Prefix</td><td><input type="text" bind:value={name_prefix}></td></tr>
+    <tr><td>Alexa-Name Prefix</td><input type="text" bind:value={alexa_name_prefix}><td></td></tr>
+    <tr><td>Room(s)</td><input type="text" bind:value={room}><td></td></tr>
+  </table>
 
   <div class="area">
     <button
