387ea43a309bdfb20d4a9369887754fd62b0a8a2
zwiebert <bertw@online.de>
2025-02-06 21:16:07 +0100

    Fix broken reactivity

--- components/app_webapp/njs/src/panes/firmware_esp32.svelte
+++ components/app_webapp/njs/src/panes/firmware_esp32.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { run } from 'svelte/legacy';
+
   import { _ } from "../services/i18n";
   import tippy from "sveltejs-tippy";
   import { GuiAcc } from "../store/app_state";
@@ -8,47 +10,55 @@
 
   import { McuGitTagNames, McuGitTagNameLatestMaster, McuGitTagNameLatestBeta } from "../store/mcu_firmware";
 
-  let fw_master = $derived({
+  let fw_master = $state({
     name: $_("firmware.latest_master"),
     input: "none",
     get_ota_name: () => {
       return "tag:master";
     },
-    version : $McuGitTagNameLatestMaster,
   });
 
-  let fw_beta = $derived({
+  let fw_beta = $state({
     name: $_("firmware.latest_beta"),
     input: "none",
     get_ota_name: () => {
       return "tag:beta";
     },
-    version : $McuGitTagNameLatestBeta,
   });
 
-  let fw_version = $derived({
+  let fw_version = $state({
     name: $_("firmware.version"),
     input: "select",
     get_ota_name: () => {
       return "tag:" + fw_version.version;
     },
-    version : $McuGitTagNames[0],
-    values : $McuGitTagNames,
   });
-  
-  let fw_url = {
-    name: $_("firmware.url"),
-    input: "input",
-    url: "http://192.168.1.76:3005/tronferno-mcu.bin",
-    get_ota_name: () => {
-      return fw_url.url;
-    },
-  };
 
-  let fwbtns = $derived([fw_master, fw_beta, fw_version, ...misc.DISTRO? [] : [fw_url]]);
+  let fwbtns = $state([fw_master, fw_beta, fw_version]);
 
+  run(() => {
+    fw_master.version = $McuGitTagNameLatestMaster;
+    fw_beta.version = $McuGitTagNameLatestBeta;
+    fw_version.values = $McuGitTagNames;
+    fwbtns = fwbtns;
+  });
 
+  let fw_version_value0 = $derived($McuGitTagNames[0]);
+  run(() => {
+    fw_version.version = fw_version_value0;
+  });
 
+  if (!misc.DISTRO) {
+    let fw_url = {
+      name: $_("firmware.url"),
+      input: "input",
+      url: "http://192.168.1.76:3005/tronferno-mcu.bin",
+      get_ota_name: () => {
+        return fw_url.url;
+      },
+    };
+    fwbtns.push(fw_url);
+  }
 </script>
 
 {#if $GuiAcc.ota}
